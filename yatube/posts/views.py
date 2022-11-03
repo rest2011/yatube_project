5196a099c55dbd898d2716debe28f3a991cd3e8a
Ринат Хаматьяров <hamatrin@gmail.com>
2022-11-03 19:52:18 +0500

    Созданы модели, админка, добавлены записи, изменены шаблоны

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,18 +3,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Импортируем загрузчик.
 from django.template import loader
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 
 # Главная страница
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 
 # Страница со списком мороженого
 def group_posts(request, slug):
-    return HttpResponse('Список постов по группам')
-
-# Страница со списком мороженого
-def group_posts(request, slug):
-    return HttpResponse('Список постов по группам')
+    template = 'posts/group_list.html'
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    title = 'Здесь будет информация о группах проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
